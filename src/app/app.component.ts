c7483a66622363566a732e1ebf92ed26e5d8939e
siasree <csvidya455@gmail.com>
2019-09-15 17:31:49 +0530

    first-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,68 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'filter-popup';
+  radios:any[]=[{
+    "radio":"India"
+  },
+  {
+    "radio":"Australia"
+  },
+  {
+    "radio":"USA"
+  },
+  {
+    "radio":"UK"
+  },
+  {
+    "radio":"China"
+  },
+  {
+    "radio":"Japan"
+  },
+  {
+    "radio":"Canada"
+  }]
+  boxes:any[]=[{
+    "box":"Bangalore"
+  },
+  {
+    "box":"Chennai"
+  },
+  {
+    "box":"Hyderabad"
+  },
+  {
+    "box":"Delhi"
+  },
+  {
+    "box":"Pune"
+  },
+  {
+    "box":"Mumbai"
+  },
+  {
+    "box":"Kolkata"
+  }
+  // {
+  //   "box":"Bangalore"
+  // },
+  // {
+  //   "box":"Chennai"
+  // },
+  // {
+  //   "box":"Hyderabad"
+  // },
+  // {
+  //   "box":"Delhi"
+  // },
+  // {
+  //   "box":"Pune"
+  // },
+  // {
+  //   "box":"Mumbai"
+  // },
+  // {
+  //   "box":"Kolkata"
+  // }
+]
 }
